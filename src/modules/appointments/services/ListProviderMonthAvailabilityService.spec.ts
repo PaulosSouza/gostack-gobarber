b47c9b693615d35b878b11af8cfd048d182f4aa2
Paulo Henrique <phferreirasouza106@gmail.com>
2020-10-13 20:51:55 -0300

    doing: Filtrando agendamentos por mês

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,44 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService();
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+  });
+
+  it('should be able to list the providers', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    const availability = listProviderMonthAvailability.execute({
+      user_id: 'user',
+      month: 5,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, availability: true },
+        { day: 20, availability: false },
+        { day: 21, availability: false },
+        { day: 22, availability: true },
+      ]),
+    );
+  });
+});
