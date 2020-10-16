985bf1487efb91fe690118ba2e41afeeab7ca5b8
Paulo Henrique <phferreirasouza106@gmail.com>
2020-10-16 06:55:43 -0300

    done: Criação do agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,6 +15,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123124',
       provider_id: '123123',
     });
 
@@ -28,12 +29,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '123124',
       provider_id: '123123',
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '123124',
         provider_id: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
