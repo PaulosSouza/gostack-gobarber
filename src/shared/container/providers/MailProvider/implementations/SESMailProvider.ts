39d4633f2df3ec98c1cbef2a285550d0a9bde3ae
Paulo Henrique <phferreirasouza106@gmail.com>
2020-12-02 06:45:47 -0300

    done: Emails pelo Amazon SES

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,54 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import aws from 'aws-sdk';
+import { inject, injectable } from 'tsyringe';
+
+import mailConfig from '@config/mail';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'us-west-1',
+        credentials: {
+          accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
+          secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+        },
+      }),
+    });
+  }
+
+  public async sendMail({
+    from,
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      from: {
+        name: from?.name || name,
+        address: email,
+      },
+      to: {
+        name: to.name,
+        address: 'phferreirasouza106@gmail.com',
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
+  }
+}
+export default SESMailProvider;
